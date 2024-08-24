668adbd029a09ac0276e44473f2b78328bb836a9
noutetu <beyoburo@gmail.com>
2024-08-24 13:34:58 +0900

    シーズン2ポケモン 5
    animatorコンポーネントを止めるための独自コンポーネントの準備

---
+++ Assets/Scripts/Utility/SpriteAnimator.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator
+{
+    //Animatorを使わずに画像を切り替え
+    //描画するためのRenderer\
+    SpriteRenderer spriteRenderer;
+    //表示するための画像たち
+    List<Sprite> frames;
+    //フレームレート:どのタイミングで画像を変えるのか
+    float frameRate;
+    float timer;
+    //現在のフレーム
+    int currentFrame;
+
+    public SpriteAnimator(SpriteRenderer spriteRenderer, List<Sprite> sprites, float frameRate = 0.16f)
+    {
+        this.spriteRenderer = spriteRenderer;
+        this.frames = sprites;
+        this.frameRate = frameRate;
+    }
+
+    //アニメーション開始
+    public void Start()
+    {
+        currentFrame = 0;
+        timer = 0;
+        spriteRenderer.sprite = frames[currentFrame];
+    }
+    //アニメーションを更新する:timerがframeRateを超えたら次の画像
+    public void HundleUpdate()
+    {
+        timer += Time.deltaTime;
+        if (timer > frameRate)
+        {
+            currentFrame = (currentFrame + 1) % frames.Count; 
+            spriteRenderer.sprite = frames[currentFrame];
+            timer -= frameRate;
+        }
+    }
+
+}
